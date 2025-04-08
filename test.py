1d71ea5485250e42f936567cbfee7e0f6cbeafc9
Josh Ebe <joshebe64@gmail.com>
2025-04-07 21:25:57 -0700

    exponent
    added functionality for exponents, first number is base and second is exponent

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-operator = input("What operation would you like to perform (+, -, *, /): ")
+operator = input("What operation would you like to perform (+, -, *, /, ^): ")
 
 if operator == "+":
     result = num1 + num2
@@ -11,5 +11,7 @@ if operator == "*":
     result = num1 * num2
 if operator == "/":
     result = num1 / num2
+if operator == "^":
+    result = num1 ** num2
 
 print("Result:", result)
