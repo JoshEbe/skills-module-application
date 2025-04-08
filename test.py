db41610c5fbb31e1a520ee9dd81e4e825e76410f
Josh Ebe <joshebe64@gmail.com>
2025-04-07 21:42:02 -0700

    root
    added functionality for taking the root of a number

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ print("Welcome to this simple calculator!")
 
 #gets input from user
 num1 = float(input("Enter the first number: "))
-operator = input("What operation would you like to perform (+, -, *, /, ^): ")
+operator = input("What operation would you like to perform (+, -, *, /, ^, root): ")
 num2 = float(input("Enter the second number: "))
 
 #addition
@@ -27,6 +27,10 @@ elif operator == "/":
 #exponent
 elif operator == "^":
     result = num1 ** num2
+
+#root, num1 is base and num2 is root
+elif operator == "root":
+    result = num1 ** (1/num2)
 
 #if the user enters something not in the list as an operator
 else: 
