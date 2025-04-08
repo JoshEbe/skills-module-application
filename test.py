7194c872de38ab3924f7d75dfef00afd4c960cab
Josh Ebe <joshebe64@gmail.com>
2025-04-07 21:31:42 -0700

    introduction and order change
    added an introductory message and now asks for operator before second number

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+print("Welcome to this simple calculator!")
 
+num1 = float(input("Enter the first number: "))
 operator = input("What operation would you like to perform (+, -, *, /, ^): ")
+num2 = float(input("Enter the second number: "))
 
 if operator == "+":
     result = num1 + num2
