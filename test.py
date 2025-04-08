129f86e954b15594c2015da5ca5058135b818ad2
Josh Ebe <joshebe64@gmail.com>
2025-04-07 19:54:00 -0700

    added subtraction functionality
    Added a third user input that allows the user to switch between addition or subtraction

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-result = num1 + num2
+operator = input("plus or minus: ")
+
+if operator == "+":
+    result = num1 + num2
+if operator == "-":
+    result = num1 - num2
 
 print("Result:", result)
