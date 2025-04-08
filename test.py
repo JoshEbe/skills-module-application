6c6c0fa4eff87bf55c51869bcc7b9330b215d30c
Josh Ebe <joshebe64@gmail.com>
2025-04-07 21:23:07 -0700

    multiplication
    added functionality for multiplication to the calculator

--- test.py
+++ test.py
@@ -1,11 +1,13 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-operator = input("plus or minus: ")
+operator = input("What operation would you like to perform (+, -, *): ")
 
 if operator == "+":
     result = num1 + num2
 if operator == "-":
     result = num1 - num2
+if operator == "*":
+    result = num1 * num2
 
 print("Result:", result)
