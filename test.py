6ec6628bc5a50e3250255d8151bf5cd73dde2de0
Josh Ebe <joshebe64@gmail.com>
2025-04-07 21:34:19 -0700

    comments
    added comments to better explain code

--- test.py
+++ test.py
@@ -1,17 +1,27 @@
 print("Welcome to this simple calculator!")
 
+#gets input from user
 num1 = float(input("Enter the first number: "))
 operator = input("What operation would you like to perform (+, -, *, /, ^): ")
 num2 = float(input("Enter the second number: "))
 
+#addition
 if operator == "+":
     result = num1 + num2
+
+#subtraction
 if operator == "-":
     result = num1 - num2
+
+#multiplication
 if operator == "*":
     result = num1 * num2
+
+#division
 if operator == "/":
     result = num1 / num2
+
+#exponent
 if operator == "^":
     result = num1 ** num2
 
